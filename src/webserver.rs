6b73510ad45f2757337563b9d7f1a4c3a2906325
icelain <icelain@ice.computer>
2025-06-23 14:04:40 +0530

    clear source after interval

--- src/webserver.rs
+++ src/webserver.rs
@@ -63,8 +63,8 @@ async fn source_clear(mut source: impl Source) {
     let mut interval = tokio::time::interval(Duration::from_secs(60 * 60));
 
     loop {
-        let _ = source.empty().await;
         interval.tick().await;
+        let _ = source.empty().await;
     }
 }
 
